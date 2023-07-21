180cada50a73d46846dea57d5a0d31016b2a3d87
jbalaraman <james.balaraman@gmail.com>
2023-07-21 17:16:07 -0600

    I am in so much emotional pain

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching (drivers, driver) {
+    const findMatch = drivers.filter(drivers => drivers.toLowerCase() == driver.toLowerCase());
+    return findMatch;
+}
+
+function fuzzyMatch (drivers, driver) {
+    const findMatch = drivers.filter(function (potato) {return potato.charAt(0) == driver.charAt(0);} );
+    return findMatch;
+}
+
+function matchName (drivers, driver) {
+    const findMatch = drivers.filter(drivers => drivers.name == driver)
+    return findMatch;
+}
+
